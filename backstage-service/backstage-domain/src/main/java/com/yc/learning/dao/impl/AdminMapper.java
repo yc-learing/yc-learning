8dfbfa2bb2edf9485cefcb164574f50459a1662e
874549037@QQ.com <874549037@qq.com>
2020-12-08 21:09:01 +0800

    基础服务框架搭建

---
+++ backstage-service/backstage-domain/src/main/java/com/yc/learning/dao/impl/AdminMapper.java
@@ -0,0 +1,9 @@
+package com.yc.learning.dao.impl;
+
+import com.yc.learning.dao.MisBaseMapper;
+import com.yc.learning.entity.Admin;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AdminMapper extends MisBaseMapper<Admin> {
+}
---
+++ backstage-service/backstage-domain/src/main/java/com/yc/learning/dao/impl/CourseMapper.java
@@ -0,0 +1,9 @@
+package com.yc.learning.dao.impl;
+
+import com.yc.learning.dao.MisBaseMapper;
+import com.yc.learning.entity.Course;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CourseMapper extends MisBaseMapper<Course> {
+}
