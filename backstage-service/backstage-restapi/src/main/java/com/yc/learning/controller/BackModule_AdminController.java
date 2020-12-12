9c265374fa2341e369f121f7f84bbec15b254279
jjyyds <976244539@qq.com>
2020-12-12 21:27:09 +0800

    后台用户及管理员分页对外接口实现

---
+++ backstage-service/backstage-restapi/src/main/java/com/yc/learning/controller/BackModule_AdminController.java
@@ -0,0 +1,59 @@
+package com.yc.learning.controller;
+
+import com.google.gson.Gson;
+import com.yc.learning.domain.AdminDomain;
+import com.yc.learning.domain.PageDomain;
+import com.yc.learning.service.BackModule_AdminService;
+import com.yc.learning.util.CommonUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping("/back-admin")
+public class BackModule_AdminController {
+
+
+    @Autowired
+    private BackModule_AdminService adminService;
+
+
+
+    @RequestMapping(value = "/findByPage", method = RequestMethod.GET)
+    public CompletableFuture<String> findByPage(Integer page, Integer pageSize, Integer aid, String aname, Integer status) {
+        return CompletableFuture.supplyAsync(() -> {
+            Map<String, Object> map = new HashMap<>();
+            try {
+                AdminDomain adminDomain = new AdminDomain();
+
+                if (CommonUtils.isNotNull(page)) {
+                    adminDomain.setPage(page);
+                }
+                if (CommonUtils.isNotNull(pageSize)) {
+                    adminDomain.setPageSize(pageSize);
+                }
+                if (CommonUtils.isNotNull(aid)) {
+                    adminDomain.setAid(aid);
+                }if (CommonUtils.isNotNull(aname)) {
+                    adminDomain.setAname(aname);
+                }if (CommonUtils.isNotNull(status)) {
+                    adminDomain.setStatus(status);
+                }
+                PageDomain<AdminDomain> pageDomain = adminService.listByPage(adminDomain);
+                map.put("code", 1);
+                map.put("data", pageDomain);
+                return new Gson().toJson(map);
+            } catch (Exception e) {
+                map.put("code",0);
+                map.put("data","程序错误");
+                e.printStackTrace();
+                return new Gson().toJson(map);
+            }
+        });
+    }
+}
