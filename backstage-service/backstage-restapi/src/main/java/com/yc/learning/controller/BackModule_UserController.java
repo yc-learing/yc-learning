6545ff3b0a4db37690056c25476b7cd46abb8da7
LiYan <1982119376@qq.com>
2020-12-13 21:21:16 +0800

    后台用户及管理员client实现

--- backstage-service/backstage-restapi/src/main/java/com/yc/learning/controller/BackModule_UserController.java
+++ backstage-service/backstage-restapi/src/main/java/com/yc/learning/controller/BackModule_UserController.java
@@ -5,10 +5,10 @@ import com.yc.learning.domain.PageDomain;
 import com.yc.learning.domain.UserDomain;
 import com.yc.learning.service.BackModule_UserService;
 import com.yc.learning.util.CommonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,6 +17,9 @@ import java.util.concurrent.CompletableFuture;
 @RestController
 @RequestMapping("/back-user")
 public class BackModule_UserController {
+    private static Logger logger = LoggerFactory.getLogger(BackModule_AdminController.class);
+
+
     @Autowired(required = false)
     private BackModule_UserService userService;
 
@@ -45,6 +48,47 @@ public class BackModule_UserController {
                 e.printStackTrace();
                 return new Gson().toJson(map);
             }
+        });
+    }
+
+    //注册
+    @RequestMapping(value = "register",method = RequestMethod.POST)
+    public CompletableFuture<String> register (@RequestBody UserDomain userDomain) throws Exception {
+        return CompletableFuture.supplyAsync(() -> {
+            Map<String, Object> map = new HashMap<>();
+            try {
+                userService.insert(userDomain);
+                logger.info("新增->ID=" + userDomain.getUid());
+                map.put("code", 1);
+                map.put("data", "注册成功");
+                return new Gson().toJson(map);
+            }catch (Exception e) {
+                map.put("code",0);
+                map.put("data","注册失败");
+                e.printStackTrace();
+                return new Gson().toJson(map);
+            }
+
+        });
+    }
+
+    //删除
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public CompletableFuture<String> delete(@PathVariable Integer id) throws Exception {
+        return CompletableFuture.supplyAsync(() -> {
+            Map<String, Object> map = new HashMap<>();
+            try{
+                userService.delete(id);
+                logger.info("删除->ID=" + id);
+                map.put("code", 1);
+                return new Gson().toJson(map);
+            }catch (Exception e) {
+                map.put("code",0);
+                map.put("data","刪除失败");
+                e.printStackTrace();
+                return new Gson().toJson(map);
+            }
+
         });
     }
 }
