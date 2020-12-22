cfa85b11ab166f541961845113cf2b448896786b
874549037@QQ.com <874549037@qq.com>
2020-12-22 15:56:55 +0800

    token登录

--- backstage-service/backstage-web/src/main/java/com/yc/learning/web/controller/BackModule_AdminWebController.java
+++ backstage-service/backstage-web/src/main/java/com/yc/learning/web/controller/BackModule_AdminWebController.java
@@ -1,7 +1,9 @@
 package com.yc.learning.web.controller;
 
-import com.yc.learning.domain.AdminDomain;
-import com.yc.learning.future.BackModule_AdminFuture;
+import com.yc.learning.entity.Admin;
+import com.yc.learning.future.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,32 +14,38 @@ import java.util.concurrent.CompletableFuture;
 public class BackModule_AdminWebController {
     @Autowired(required = false)
     private BackModule_AdminFuture adminFuture;
+    private static Logger logger = LoggerFactory.getLogger(BackModule_AdminWebController.class.getName());
 
     @RequestMapping(value = "findByPage",method = RequestMethod.GET)
     public CompletableFuture<String> findByPage(@RequestParam(value = "page",required = false) Integer page, @RequestParam(value = "pageSize",required = false) Integer pageSize, @RequestParam(value = "aname",required = false) String aname) {
         return adminFuture.findByPage(page, pageSize, aname);
     }
 
-    //注册
-    @RequestMapping(value = "register",method = RequestMethod.POST)
-    public CompletableFuture<String> register (@RequestBody AdminDomain adminDomain){
-        return adminFuture.insert(adminDomain);
-    }
-
-    //删除
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public CompletableFuture<String> delete(@PathVariable Integer id){
-        return adminFuture.delete(id);
-    }
-
-
-
-    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    @RequestMapping(value = "update", method = RequestMethod.POST)
     public CompletableFuture<String> update(
             @RequestParam(value = "aid") Integer aid,
-            @RequestParam(value = "value") Object value,
+            @RequestParam(value = "value") String value,
             @RequestParam(value = "field") String field){
-
+        System.out.println("修改Webcontroller");
         return adminFuture.update(aid,value,field);
     }
+
+
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public CompletableFuture<String> login(
+            @RequestBody Admin admin){
+        logger.info(admin+"用户登录");
+        return adminFuture.login(admin);
+    }
+
+
+    @RequestMapping(value = "check", method = RequestMethod.POST)
+    public CompletableFuture<String> check(
+            @RequestParam(value = "token") String token){
+        logger.info("token为："+token);
+        return adminFuture.check(token);
+    }
+
+
+
 }
