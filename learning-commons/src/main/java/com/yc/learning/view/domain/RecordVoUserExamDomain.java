ae80f4f0f42bda8ece0f1458ed604e8572ee0b5b
jjyyds <976244539@qq.com>
2020-12-30 21:15:34 +0800

    答辩不丢脸的整合

---
+++ learning-commons/src/main/java/com/yc/learning/view/domain/RecordVoUserExamDomain.java
@@ -0,0 +1,20 @@
+package com.yc.learning.view.domain;
+
+import com.yc.learning.domain.PageDomain;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RecordVoUserExamDomain extends PageDomain {
+    private Integer rid;
+    private Integer uid;
+    private String uname;
+    private String useranswer;
+    private Double grade;
+    private Integer exid;
+    private String ename;
+    private String eids;
+}
